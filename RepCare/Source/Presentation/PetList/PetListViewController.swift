190ee899a2f53ccaf6622fbe6f52b148386c8629
Kim junhwan <tazanboy1@naver.com>
2023-09-29 23:28:28 +0900

    [feat] 프로젝트 최소 버전 설정 및 세로모드만 지원, 코드 베이스 설정

---
+++ RepCare/Source/Presentation/PetList/PetListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PetListViewController.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/09/29.
+//
+
+import UIKit
+
+class PetListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
