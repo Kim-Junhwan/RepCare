2b8ef7c0351e0ed491c51c4a9891240add573a0b
Kim junhwan <tazanboy1@naver.com>
2023-09-29 23:30:15 +0900

    [feat] 기본 색상 설정

---
+++ RepCare/Source/Presentation/Utils/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/09/29.
+//
+
+import UIKit
+
+enum Color {
+    static let deepGreen = UIColor(named: "DeepGreen")
+    static let lightDeepGreen = UIColor(named: "LightDeepGreen")
+    static let lightGreen = UIColor(named: "LightGreen")
+    static let brightLightGreen = UIColor(named: "BrightLightGreen")
+}
