e29a935ff6a28722603032f45fef5a164ed33ee7
Kim junhwan <tazanboy1@naver.com>
2023-10-09 03:26:55 +0900

    [feat] 애완동물 종 선택 ViewModel petClass, petSpecies 선택 시 하위 세부사항 가져오는 기능 구현

--- RepCare/Source/Presentation/ClassSpeciesMorph/ViewModel/ClassSpeciesMorphViewModel.swift
+++ RepCare/Source/Presentation/ClassSpeciesMorph/ViewModel/ClassSpeciesMorphViewModel.swift
@@ -31,6 +31,7 @@ enum Section: Int, CaseIterable {
 
 struct Item: Hashable {
     let title: String
+    let id: String
     private let identifier = UUID()
 }
 
@@ -38,8 +39,10 @@ final class ClassSpeciesMorphViewModel {
     
     private let repository: SpeciesRepository
     
-    let selectPetClass: PublishRelay<Item> = .init()
-    let fetchPetClassList: PublishSubject<[PetClassItemViewModel]> = .init()
+    let selectPetClass: PublishRelay<PetClassItemViewModel> = .init()
+    let selectSpecies: PublishRelay<Int> = .init()
+    let speciesList: BehaviorRelay<[Section: [Item]]> = .init(value: [.detailSpecies:[],.morph:[],.petClass:[],.species:[]])
+    
     
     init(repository: SpeciesRepository) {
         self.repository = repository
@@ -47,10 +50,43 @@ final class ClassSpeciesMorphViewModel {
     
     func viewDidLoad() {
         fetchPetClass()
+        bind()
+    }
+    
+    func removeSection(sectionIndex: Int) {
+        guard let section = Section(rawValue: sectionIndex+1) else { return }
+        updateList(section: section, data: [])
     }
     
     private func fetchPetClass() {
-        let fetchList = repository.fetchPetClass().map {PetClassItemViewModel(petClass: $0)}
-        fetchPetClassList.onNext(fetchList)
+        let fetchPetClass = repository.fetchPetClass().map { PetClassItemViewModel(petClass: $0) }
+        var origin = speciesList.value
+        origin[.petClass] = fetchPetClass.map { Item(title: $0.title, id: "") }
+        speciesList.accept(origin)
+    }
+    
+    private func updateList(section: Section, data: [Item]) {
+        var origin = speciesList.value
+        origin[section] = data
+        for sectionValue in (section.rawValue+1)..<Section.allCases.count {
+            origin[Section(rawValue: sectionValue)!] = []
+        }
+        speciesList.accept(origin)
+    }
+    
+    private func bind() {
+        selectPetClass.subscribe { item in
+            guard let petClass = item.element?.toDomain() else { return }
+            let fetchSpecies = self.repository.fetchSpecies(petClass: petClass)
+            self.updateList(section: .species, data: fetchSpecies.map { Item(title: $0.species, id: $0.id) })
+            
+        }
+        
+        selectSpecies.subscribe { item in
+            guard let species = item.element else { return }
+            guard let select = self.speciesList.value[.species]?[species] else { return }
+            let fetchDetailSpecies = self.repository.fetchDetailSpecies(species: .init(id: select.id, species: select.title))
+            self.updateList(section: .detailSpecies, data: fetchDetailSpecies.map { .init(title: $0.detailSpecies, id: $0.id) })
+        }
     }
 }
