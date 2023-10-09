b760d4ee0b05f3582d826a0a461a9ec3532e66e3
Kim junhwan <tazanboy1@naver.com>
2023-10-10 00:31:47 +0900

    [feat] 메인 화면 네비게이션 컨트롤러 추가

--- RepCare/Source/Application/SceneDelegate.swift
+++ RepCare/Source/Application/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = PetListViewController()
+        
+        let nvc = UINavigationController(rootViewController: PetListViewController())
+        window?.rootViewController = nvc
         window?.makeKeyAndVisible()
     }
 
