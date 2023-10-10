d0727c870606ba13611722aaaf2ece4cbec1938b
Kim junhwan <tazanboy1@naver.com>
2023-10-10 22:15:13 +0900

    [feat] RegisterPetViewModel 구현

---
+++ RepCare/Source/Presentation/RegisterPet/ViewModel/RegisterPetViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterPetViewModel.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/10/05.
+//
+
+import Foundation
+import RxRelay
+import RxSwift
+
+final class RegisterPetViewModel {
+    let petName: BehaviorRelay<String> = .init(value: "")
+    let overPetSpecies: PublishRelay<PetOverSpeciesModel> = .init()
+    let gender: PublishRelay<GenderModel> = .init()
+    let adoptionDate: PublishRelay<Date> = .init()
+    var hatchDate: PublishRelay<Date?> = .init()
+    var currentWeight: PublishRelay<Double?> = .init()
+    
+    lazy var canRegister = BehaviorRelay.combineLatest(petName, overPetSpecies, adoptionDate, gender) { name, _, _, _ in
+        return !name.isEmpty
+    }
+}
