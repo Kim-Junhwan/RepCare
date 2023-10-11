ed6e04cff3c671c765b4e34b4da5adbda77af3ae
Kim junhwan <tazanboy1@naver.com>
2023-10-11 23:25:09 +0900

    [refactor] Realm Pet 속성 변경

--- RepCare/Source/Data/Object/PetObject.swift
+++ RepCare/Source/Data/Object/PetObject.swift
@@ -12,17 +12,27 @@ class PetObject: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var name: String
     @Persisted var gender: GenderType
+    @Persisted var petClass: PetClassObject?
+    @Persisted var petSpecies: PetSpeciesObject?
     @Persisted var detailSpecies: DetailSpeciesObject?
-    @Persisted var adoptionDate: Date
-    @Persisted var birthDate: Date
-    @Persisted var weights: List<WeightObject>
+    @Persisted var morph: MorphObject?
     
-    convenience init(name: String, gender: GenderType, detailSpecies: DetailSpeciesObject, birthDate: Date, adoptionDate: Date) {
+    @Persisted var adoptionDate: Date
+    @Persisted var weights: List<WeightObject>
+    @Persisted var birthDate: Date?
+    
+    convenience init(name:String, gender: GenderType, petClass: PetClassObject, petSpecies: PetSpeciesObject, detailSpecies: DetailSpeciesObject?, morph: MorphObject?, adoptionDate: Date, weights: WeightObject?, birthDate: Date?) {
         self.init()
         self.name = name
         self.gender = gender
+        self.petClass = petClass
+        self.petSpecies = petSpecies
         self.detailSpecies = detailSpecies
+        self.morph = morph
         self.adoptionDate = adoptionDate
+        if let weights = weights {
+            self.weights.append(weights)
+        }
         self.birthDate = birthDate
     }
 }
