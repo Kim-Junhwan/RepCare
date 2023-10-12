599e05160f5dca023cf06e50e0597bf806b9f698
Kim junhwan <tazanboy1@naver.com>
2023-10-12 21:25:17 +0900

    [feat] 등록 값 BehaviorRelay로 변경 및 등록기능 추가

--- RepCare/Source/Presentation/RegisterPet/ViewModel/RegisterPetViewModel.swift
+++ RepCare/Source/Presentation/RegisterPet/ViewModel/RegisterPetViewModel.swift
@@ -13,13 +13,39 @@ final class RegisterPetViewModel {
     
     let petImageList: BehaviorRelay<[PetImageItem]> = .init(value: [])
     let petName: BehaviorRelay<String> = .init(value: "")
-    let overPetSpecies: PublishRelay<PetOverSpeciesModel> = .init()
-    let gender: PublishRelay<GenderModel> = .init()
-    let adoptionDate: PublishRelay<Date> = .init()
-    var hatchDate: PublishRelay<Date?> = .init()
-    var currentWeight: PublishRelay<Double?> = .init()
+    let overPetSpecies: BehaviorRelay<PetOverSpeciesModel?> = .init(value: nil)
+    let gender: BehaviorRelay<GenderModel?> = .init(value: nil)
+    let adoptionDate: BehaviorRelay<Date?> = .init(value: nil)
+    var hatchDate: BehaviorRelay<Date?> = .init(value: nil)
+    var currentWeight: BehaviorRelay<Double?> = .init(value: nil)
     
-    lazy var canRegister = BehaviorRelay.combineLatest(petName, overPetSpecies, adoptionDate, gender) { name, _, _, _ in
-        return !name.isEmpty
+    lazy var canRegister = BehaviorRelay.combineLatest(petName, overPetSpecies, adoptionDate, gender) { name, petSpecies, adopDate, gender in
+        return !name.isEmpty && (gender != nil) && (petSpecies != nil) && (adopDate != nil)
+    }
+    
+    let registerUseCase = DefaultRegisterPetUseCase(petRepository: DefaultPetRepository(petStorage: RealmPetStorage(), speciesStroage: RealmSpeciesStorage()), petImageRepository: FileManaerPetImageRepository())
+    
+    func register() {
+        let imageListData = petImageList.value.map({ image in
+            guard let imageData = image.image.pngData() else { fatalError() }
+            return imageData
+        })
+        do {
+            guard let petClass = overPetSpecies.value?.petClass else { return }
+            guard let species = overPetSpecies.value?.petSpecies else { return }
+            guard let adoptionDate = adoptionDate.value else { return }
+            guard let gender = gender.value else { return }
+            let weight: Weight?
+            if currentWeight.value == nil {
+                weight = nil
+            } else {
+                guard let weightNum = currentWeight.value else { return }
+                weight = .init(date: adoptionDate, weight: weightNum)
+            }
+            try registerUseCase.registerPet(request: .init(name: petName.value, imageDataList: imageListData, petClass: petClass.toDomain(), petSpecies: species.toDomain(), detailSpecies: overPetSpecies.value?.detailSpecies?.toDomain(), morph: overPetSpecies.value?.morph?.toDomain(), adoptionDate: adoptionDate, birthDate: hatchDate.value, gender: gender.toDomain(), weight: weight))
+        } catch {
+            print("error")
+        }
+        
     }
 }
