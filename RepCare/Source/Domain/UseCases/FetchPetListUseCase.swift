ecdf62a9713bc484d5408e2311eafe8a9299c8e8
Kim junhwan <tazanboy1@naver.com>
2023-10-12 23:08:19 +0900

    [feat] 등록된 펫을 가져오는 UseCase 구현

---
+++ RepCare/Source/Domain/UseCases/FetchPetListUseCase.swift
@@ -0,0 +1,39 @@
+//
+//  FetchPetSpeciesOfClassUseCase.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/10/01.
+//
+
+import Foundation
+
+protocol FetchPetListUseCase {
+    func fetchPetList(request: FetchPetListRequest) -> PetPage
+}
+
+struct FetchPetListRequest {
+    var page: Int
+    var petSpecies: Species
+    var petClass: PetClass
+    var searchKeyword: String?
+}
+
+final class DefaultFetchPetListUseCase {
+    let petRepository: PetRepository
+    
+    init(petRepository: PetRepository) {
+        self.petRepository = petRepository
+    }
+}
+
+extension DefaultFetchPetListUseCase: FetchPetListUseCase {
+    func fetchPetList(request: FetchPetListRequest) -> PetPage {
+        let fetchIndex = request.page * Rule.fetchPetCount
+        let query = PetQuery(petClass: request.petClass, species: request.petSpecies, searchKeyword: request.searchKeyword)
+        let response = petRepository.fetchPetList(query: query, start: fetchIndex)
+        
+        let currentPage = (response.start/Rule.fetchPetCount)+1
+        let totalPage = response.totalPetCount % Rule.fetchPetCount == 0 ? response.totalPetCount/Rule.fetchPetCount : (response.totalPetCount/Rule.fetchPetCount)+1
+        return PetPage(currentPage: currentPage, totalPage: totalPage, petList: response.petList)
+    }
+}
