8a594fe95e78cb49fd349ea90dcb03ac6b2e59bb
Kim junhwan <tazanboy1@naver.com>
2023-10-12 21:15:37 +0900

    [feat] FileManagerPetImageRepository 구현

---
+++ RepCare/Source/Data/File/FileManagerPetImageRepository.swift
@@ -0,0 +1,27 @@
+//
+//  PetImageRepository.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/10/12.
+//
+
+import Foundation
+
+final class FileManaerPetImageRepository: PetImageRepository {
+    func savePetImage(petId: String, petImageList: [Data]) throws {
+        guard let defaultDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let petImageDirectoryURL = defaultDirectory.appendingPathComponent(petId, conformingTo: .directory)
+        
+        try FileManager.default.createDirectory(at: petImageDirectoryURL, withIntermediateDirectories: true)
+        for images in 0..<petImageList.count {
+            let imagePath = petImageDirectoryURL.appendingPathComponent("\(images)", conformingTo: .png)
+            try petImageList[images].write(to: imagePath)
+        }
+    }
+    
+    func savePetImage(petImageList: [Data]) -> [PetImage] {
+        return []
+    }
+    
+    
+}
