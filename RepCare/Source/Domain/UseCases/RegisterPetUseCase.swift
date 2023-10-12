a42ba0bcce054774b684eb2dd799eb3381d766b9
Kim junhwan <tazanboy1@naver.com>
2023-10-12 20:58:31 +0900

    [feat] 펫 등록 UseCase 정의 및 구현

---
+++ RepCare/Source/Domain/UseCases/RegisterPetUseCase.swift
@@ -0,0 +1,53 @@
+//
+//  RegisterPetUseCase.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2023/10/11.
+//
+
+import Foundation
+
+protocol RegisterPetUseCase {
+    func registerPet(request: RegisterPetRequest) throws
+}
+
+struct RegisterPetRequest {
+    let name: String
+    let imageDataList: [Data]
+    let petClass: PetClass
+    let petSpecies: Species
+    let detailSpecies: DetailSpecies?
+    let morph: Morph?
+    let adoptionDate: Date
+    let birthDate: Date?
+    let gender: Gender
+    let weight: Weight?
+}
+
+final class DefaultRegisterPetUseCase {
+    let petRepository: PetRepository
+    let petImageRepository: PetImageRepository
+    
+    init(petRepository: PetRepository, petImageRepository: PetImageRepository) {
+        self.petRepository = petRepository
+        self.petImageRepository = petImageRepository
+    }
+}
+
+extension DefaultRegisterPetUseCase: RegisterPetUseCase {
+    func registerPet(request: RegisterPetRequest) throws {
+        let registerPetId =
+        try petRepository.registerPet(pet: .init(name: request.name,
+                                                 imagePath: request.imageDataList.enumerated().map { .init(imagePath: "\($0.offset)") }
+                                                 ,petClass: request.petClass,
+                                                 petSpecies: request.petSpecies,
+                                                 detailSpecies: request.detailSpecies,
+                                                 morph: request.morph,
+                                                 adoptionDate: request.adoptionDate,
+                                                 birthDate: request.birthDate,
+                                                 gender: request.gender,
+                                                 weight: request.weight))
+        let saveImagePathList = try petImageRepository.savePetImage(petId: registerPetId, petImageList: request.imageDataList
+        )
+    }
+}
