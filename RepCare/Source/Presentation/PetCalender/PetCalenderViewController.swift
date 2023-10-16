2381f12671b84d345c6a5a0b93260613497a97b1
Kim junhwan <tazanboy1@naver.com>
2023-10-16 13:39:07 +0900

    [feat] PetCalenderView 구현

--- RepCare/Source/Presentation/PetCalender/PetCalenderViewController.swift
+++ RepCare/Source/Presentation/PetCalender/PetCalenderViewController.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 final class PetCalenderViewController: BaseViewController {
+    let mainView = PetCalenderView()
     
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        title = "캘린더 · 타임라인"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
--- RepCare/Source/Presentation/PetCalender/View/PetCalenderView.swift
+++ RepCare/Source/Presentation/PetCalender/View/PetCalenderView.swift
@@ -9,12 +9,53 @@ import UIKit
 
 class PetCalenderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+        collectionView.register(TaskCollectionViewCell.self, forCellWithReuseIdentifier: TaskCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        setConstrains()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        addSubview(collectionView)
+    }
 
+    private func setConstrains() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    private func makeCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width: 100, height: 100)
+        return layout
+    }
+}
+
+extension PetCalenderView: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TaskCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    
 }
