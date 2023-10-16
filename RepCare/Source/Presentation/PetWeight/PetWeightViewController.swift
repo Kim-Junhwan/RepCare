c4f9f0ff8267034d5be25017163eede051cafed8
Kim junhwan <tazanboy1@naver.com>
2023-10-16 13:38:02 +0900

    [feat] PetWeightController title 설정

--- RepCare/Source/Presentation/PetWeight/PetWeightViewController.swift
+++ RepCare/Source/Presentation/PetWeight/PetWeightViewController.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+final class PetWeightViewController: BaseViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        title = "체중"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
