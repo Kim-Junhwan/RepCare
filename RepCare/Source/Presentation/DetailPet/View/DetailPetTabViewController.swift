540297c5d0bf6466445c079cb6852c688dfb76c4
Kim junhwan <tazanboy1@naver.com>
2023-10-16 13:33:16 +0900

    [feat] Tabman으로 TabBarViewController 구현

--- RepCare/Source/Presentation/DetailPet/View/DetailPetTabViewController.swift
+++ RepCare/Source/Presentation/DetailPet/View/DetailPetTabViewController.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Tabman
+import Pageboy
+import UIKit
+
+protocol CustomTabBarDelegate: AnyObject {
+    func pageViewController(_ currentViewController: UIViewController?, didselectPageAt index: Int)
+}
+
+class DetailPetTabViewController: TabmanViewController {
+    
+    private var viewControllers = [UIViewController(), UIViewController()]
+    
+    lazy var bar: TMBar = {
+        let bar = TMBar.ButtonBar()
+        bar.backgroundView.style = .flat(color: .systemBackground)
+        bar.layout.transitionStyle = .snap
+        return bar
+    }()
+    
+    weak var delegate: CustomTabBarDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setTapBar(datasource: TMBarDataSource) {
+        addBar(bar, dataSource: datasource, at: .top)
+    }
+    
+    override func pageboyViewController(_ pageboyViewController: PageboyViewController, didScrollToPageAt index: TabmanViewController.PageIndex, direction: PageboyViewController.NavigationDirection, animated: Bool) {
+        super.pageboyViewController(pageboyViewController, didScrollToPageAt: index, direction: direction, animated: animated)
+        delegate?.pageViewController(dataSource?.viewController(for: self, at: index), didselectPageAt: index)
+    }
+}
