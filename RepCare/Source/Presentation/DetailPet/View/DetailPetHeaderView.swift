9d94d8cee9ba29f6fb8bcf2f6ab8e318ab941315
Kim junhwan <tazanboy1@naver.com>
2023-10-16 13:37:16 +0900

    [feat] PetHeaderView 구현

--- RepCare/Source/Presentation/DetailPet/View/DetailPetHeaderView.swift
+++ RepCare/Source/Presentation/DetailPet/View/DetailPetHeaderView.swift
@@ -7,14 +7,97 @@
 
 import UIKit
 
+
 class DetailPetHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let imagePageViewController: UIPageViewController = {
+        let pageView = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageView.view.backgroundColor = .red
+        return pageView
+    }()
+    let pageControl: UIPageControl = UIPageControl()
+    lazy var onStackView: UIStackView = {
+        let stackView = UIStackView()
+         stackView.addArrangedSubview(nameStackView)
+        stackView.addArrangedSubview(adoptionLabel)
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+         return stackView
+     }()
+    
+    lazy var nameStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(genderImageView)
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    let nameLabel: UILabel = UILabel()
+    let genderImageView: UIImageView = .init()
+    let adoptionLabel: UILabel = UILabel()
+    
+    lazy var speciesStackView: UIStackView = {
+        let stackView = UIStackView()
+         stackView.addArrangedSubview(speciesLabel)
+         stackView.addArrangedSubview(morphLabel)
+         stackView.axis = .horizontal
+         stackView.spacing = 5
+         return stackView
+     }()
+    let speciesLabel: UILabel = UILabel()
+    let morphLabel: UILabel = UILabel()
+    
+    lazy var petInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+         stackView.addArrangedSubview(onStackView)
+         stackView.addArrangedSubview(speciesStackView)
+         stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillEqually
+         stackView.spacing = 5
+         return stackView
+     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nameLabel.text = "이름"
+        adoptionLabel.text = "입양한지 100일째"
+        speciesLabel.text = "고양이"
+        morphLabel.text = "치즈냥이"
+        configureView()
+        setConstraints()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        addSubview(imagePageViewController.view)
+        addSubview(pageControl)
+        addSubview(petInfoStackView)
+    }
+    
+    private func setConstraints() {
+        imagePageViewController.view.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.8)
+        }
+        pageControl.snp.makeConstraints { make in
+            make.centerX.equalTo(imagePageViewController.view.snp.centerX)
+            make.bottom.equalTo(imagePageViewController.view.snp.bottom)
+        }
+        petInfoStackView.snp.makeConstraints { make in
+            make.top.equalTo(imagePageViewController.view.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        onStackView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+        }
+        genderImageView.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.width.equalTo(genderImageView.snp.height).multipliedBy(1.0)
+        }
+    }
 }
