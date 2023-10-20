03f09d12ee390dcca76f81359e15c7ef19e7833b
Kim junhwan <tazanboy1@naver.com>
2023-10-20 23:14:46 +0900

    [feat] UIColor 현재 색상보다 밝은 색상 반환하는 기능 추가

--- RepCare/Source/Presentation/Utils/Color.swift
+++ RepCare/Source/Presentation/Utils/Color.swift
@@ -12,4 +12,21 @@ enum Color {
     static let lightDeepGreen = UIColor(named: "LightDeepGreen")
     static let lightGreen = UIColor(named: "LightGreen")
     static let brightLightGreen = UIColor(named: "BrightLightGreen")
+    static let lightPink = UIColor(named: "LightPink")
+    static let middleGray = UIColor(named: "MiddleGray")
+    static let lightYellow = UIColor(named: "LightYellow")
+    static let oceanBlue = UIColor(named: "OceanBlue")
+}
+
+extension UIColor {
+    func adjustBrightness(factor: CGFloat) -> UIColor {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        self.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        red = min(1.0, red * factor)
+        green = min(1.0, green * factor)
+        blue = min(1.0, blue * factor)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
