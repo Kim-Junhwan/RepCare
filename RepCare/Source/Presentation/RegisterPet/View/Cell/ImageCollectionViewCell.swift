88f4bfb2589ee3ac1ec226d8bb7f03c6afa821a7
Kim junhwan <tazanboy1@naver.com>
2023-10-24 22:51:45 +0900

    [refcator] 펫 추가 화면 이미지 셀 디자인 변경 및 삭제 버튼 추가

--- RepCare/Source/Presentation/RegisterPet/View/Cell/ImageCollectionViewCell.swift
+++ RepCare/Source/Presentation/RegisterPet/View/Cell/ImageCollectionViewCell.swift
@@ -15,6 +15,17 @@ class ImageCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
+    let deleteButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        var imageConfig = UIImage.SymbolConfiguration(pointSize: 6, weight: .light)
+        config.image = UIImage(systemName: "xmark", withConfiguration: imageConfig)
+        config.baseForegroundColor = .white
+        config.baseBackgroundColor = .black
+        let button = UIButton(configuration: config)
+        button.clipsToBounds = true
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureView()
@@ -26,12 +37,34 @@ class ImageCollectionViewCell: UICollectionViewCell {
     
     private func configureView() {
         contentView.addSubview(imageView)
+        contentView.addSubview(deleteButton)
+        deleteButton.addTarget(self, action: #selector(tapDelete), for: .touchUpInside)
         imageView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.leading.bottom.equalToSuperview()
+            make.width.height.equalToSuperview().multipliedBy(0.9)
         }
+        deleteButton.snp.makeConstraints { make in
+            make.centerX.equalTo(imageView.snp.trailing)
+            make.centerY.equalTo(imageView.snp.top)
+            make.width.height.equalToSuperview().multipliedBy(0.2)
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        deleteButton.layer.cornerRadius = deleteButton.bounds.height/2
     }
     
     func configureCell(petImage: PetImageItem) {
         imageView.image = petImage.image
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.layer.cornerRadius = 10.0
+    }
+    
+    @objc private func tapDelete() {
+        print("DELETE")
     }
 }
