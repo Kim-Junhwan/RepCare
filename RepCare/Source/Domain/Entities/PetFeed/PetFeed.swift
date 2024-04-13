75cae4bb6b0bd1852b9cf226f03ca570bdc53b8f
Kim junhwan <tazanboy1@naver.com>
2024-04-13 23:59:12 +0900

    [feat] 먹이, 영양제 객체 정의

---
+++ RepCare/Source/Domain/Entities/PetFeed/PetFeed.swift
@@ -0,0 +1,13 @@
+//
+//  PetFeed.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2024/04/13.
+//
+
+import Foundation
+
+struct PetFeed {
+    let name: String
+    let image: Data
+}
---
+++ RepCare/Source/Domain/Entities/PetFeed/PetSupplement.swift
@@ -0,0 +1,8 @@
+//
+//  PetSupplement.swift
+//  RepCare
+//
+//  Created by JunHwan Kim on 2024/04/13.
+//
+
+import Foundation
