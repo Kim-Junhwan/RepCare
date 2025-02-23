8960484760d9d5968a40913b3a87c611e5393332
김준환(Junhwan.Kim) <tazanboy1@inavi.kr>
2025-02-23 22:31:46 +0900

    fix: swiftUI 프리뷰 오류 수정

--- RepCare/Source/Application/AppDelegate.swift
+++ RepCare/Source/Application/AppDelegate.swift
@@ -13,6 +13,9 @@ import FirebaseMessaging
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+            return true
+        }
         copyRealm()
         FirebaseApp.configure()
         Analytics.logEvent(AnalyticsEventAppOpen, parameters: nil)
